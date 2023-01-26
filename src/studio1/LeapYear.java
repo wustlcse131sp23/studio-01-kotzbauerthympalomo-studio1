316853a3dc0532a6ba774e0b7b80205c6bfa668a
kevink <kevink@localhost>
2023-01-26 12:32:43 -0600

    Kotzbauer_2

---
+++ src/studio1/LeapYear.java
@@ -0,0 +1,16 @@
+package studio1;
+
+import java.util.Scanner;
+
+public class LeapYear {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter a year to determine if it is a leap year");
+		int year = in.nextInt();
+		boolean leapYear = (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0);
+		System.out.print(year + " is a leap year: " + leapYear);
+	}
+
+}
